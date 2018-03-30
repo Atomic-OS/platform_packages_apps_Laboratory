DirtyUnicorns/android_packages_apps_DU-Tweaks

Choose a custom editor app for screenshot notification edit button [2/2]
Change-Id: I55039dfd1f5eda2bbc219fe127644075b77d5008

src/com/dirtyunicorns/tweaks/fragments/Miscellaneous.java
@@ -16,32 +16,104 @@
 
 package com.dirtyunicorns.tweaks.fragments;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.ContentResolver;
+import android.content.Intent;
+import android.content.res.Resources;
 import android.os.Bundle;
-import android.support.v7.preference.PreferenceCategory;
+import android.os.Handler;
 import android.support.v7.preference.ListPreference;
 import android.support.v7.preference.Preference;
 import android.support.v7.preference.PreferenceScreen;
-import android.support.v7.preference.Preference.OnPreferenceChangeListener;
-import android.support.v14.preference.SwitchPreference;
 import android.provider.Settings;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ListView;
+import android.widget.Toast;
 
+import com.android.internal.logging.nano.MetricsProto;
+import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
 import com.android.settings.Utils;
 
-import com.android.internal.logging.nano.MetricsProto;
+import com.dirtyunicorns.tweaks.preferences.ScreenshotEditPackageListAdapter;
+import com.dirtyunicorns.tweaks.preferences.ScreenshotEditPackageListAdapter.PackageItem;
 
-public class Miscellaneous extends SettingsPreferenceFragment implements Preference.OnPreferenceChangeListener {
+
+
+public class Miscellaneous extends SettingsPreferenceFragment implements
+        Preference.OnPreferenceChangeListener, Preference.OnPreferenceClickListener {
+
+    private static final int DIALOG_SCREENSHOT_EDIT_APP = 1;
+
+    private Preference mScreenshotEditAppPref;
+    private ScreenshotEditPackageListAdapter mPackageAdapter;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.miscellaneous);
+
+        mPackageAdapter = new ScreenshotEditPackageListAdapter(getActivity());
+        mScreenshotEditAppPref = findPreference("screenshot_edit_app");
+        mScreenshotEditAppPref.setOnPreferenceClickListener(this);
+    }
+
+    @Override
+    public Dialog onCreateDialog(int dialogId) {
+        switch (dialogId) {
+            case DIALOG_SCREENSHOT_EDIT_APP: {
+                Dialog dialog;
+                AlertDialog.Builder alertDialog = new AlertDialog.Builder(getActivity());
+                final ListView list = new ListView(getActivity());
+                list.setAdapter(mPackageAdapter);
+                alertDialog.setTitle(R.string.profile_choose_app);
+                alertDialog.setView(list);
+                dialog = alertDialog.create();
+                list.setOnItemClickListener(new OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                        // Add empty application definition, the user will be able to edit it later
+                        PackageItem info = (PackageItem) parent.getItemAtPosition(position);
+                        Settings.System.putString(getActivity().getContentResolver(),
+                                Settings.System.SCREENSHOT_EDIT_USER_APP, info.packageName);
+                        dialog.cancel();
+                    }
+                });
+                return dialog;
+            }
+         }
+        return super.onCreateDialog(dialogId);
+    }
+
+    @Override
+    public int getDialogMetricsCategory(int dialogId) {
+        switch (dialogId) {
+            case DIALOG_SCREENSHOT_EDIT_APP:
+                return MetricsEvent.DIRTYTWEAKS;
+            default:
+                return 0;
+        }
     }
 
     @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
         return false;
+    }
+
+    @Override
+    public boolean onPreferenceClick(Preference preference) {
+        // Don't show the dialog if there are no available editor apps
+        if (preference == mScreenshotEditAppPref && mPackageAdapter.getCount() > 0) {
+            showDialog(DIALOG_SCREENSHOT_EDIT_APP);
+        } else {
+            Toast.makeText(getActivity(), getActivity().getString(R.string.screenshot_edit_app_no_editor),
+                    Toast.LENGTH_LONG).show();
+        }
+        return true;
     }
 
     @Override

src/com/dirtyunicorns/tweaks/preferences/ScreenshotEditPackageListAdapter.java
@@ -1,0 +1,151 @@
+/*
+ * Copyright (C) 2012-2014 The CyanogenMod Project
+ * Copyright (C) 2018 Dirty Unicorns
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.dirtyunicorns.tweaks.preferences;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.graphics.drawable.Drawable;
+import android.os.Handler;
+import android.os.Message;
+import android.provider.MediaStore;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+
+import com.android.settings.R;
+
+public class ScreenshotEditPackageListAdapter extends BaseAdapter implements Runnable {
+    private PackageManager mPm;
+    private LayoutInflater mInflater;
+    private final List<PackageItem> mInstalledPackages = new LinkedList<PackageItem>();
+
+    private final Handler mHandler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            PackageItem item = (PackageItem) msg.obj;
+            int index = Collections.binarySearch(mInstalledPackages, item);
+            if (index < 0) {
+                mInstalledPackages.add(-index - 1, item);
+            }
+            notifyDataSetChanged();
+        }
+    };
+
+    public static class PackageItem implements Comparable<PackageItem> {
+        public final String packageName;
+        public final CharSequence title;
+        public final Drawable icon;
+
+        PackageItem(String packageName, CharSequence title, Drawable icon) {
+            this.packageName = packageName;
+            this.title = title;
+            this.icon = icon;
+        }
+
+        @Override
+        public int compareTo(PackageItem another) {
+            int result = title.toString().compareToIgnoreCase(another.title.toString());
+            return result != 0 ? result : packageName.compareTo(another.packageName);
+        }
+    }
+
+    public ScreenshotEditPackageListAdapter(Context context) {
+        mPm = context.getPackageManager();
+        mInflater = LayoutInflater.from(context);
+        reloadList();
+    }
+
+    @Override
+    public int getCount() {
+        synchronized (mInstalledPackages) {
+            return mInstalledPackages.size();
+        }
+    }
+
+    @Override
+    public PackageItem getItem(int position) {
+        synchronized (mInstalledPackages) {
+            return mInstalledPackages.get(position);
+        }
+    }
+
+    @Override
+    public long getItemId(int position) {
+        synchronized (mInstalledPackages) {
+            // packageName is guaranteed to be unique in mInstalledPackages
+            return mInstalledPackages.get(position).packageName.hashCode();
+        }
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ViewHolder holder;
+        if (convertView != null) {
+            holder = (ViewHolder) convertView.getTag();
+        } else {
+            convertView = mInflater.inflate(R.layout.applist_preference_icon, null, false);
+            holder = new ViewHolder();
+            convertView.setTag(holder);
+            holder.title = (TextView) convertView.findViewById(com.android.internal.R.id.title);
+            holder.icon = (ImageView) convertView.findViewById(R.id.icon);
+        }
+
+        PackageItem applicationInfo = getItem(position);
+        holder.title.setText(applicationInfo.title);
+        holder.icon.setImageDrawable(applicationInfo.icon);
+
+        return convertView;
+    }
+
+    private void reloadList() {
+        mInstalledPackages.clear();
+        new Thread(this).start();
+    }
+
+    @Override
+    public void run() {
+        Intent intent = new Intent();
+        intent.setAction(Intent.ACTION_EDIT);
+        // setData is needed because some apps like PhotoEditor need an uri for the EDIT intent action,
+        // otherwise they don't answer to the intent query. So we give them the generic EXTERNAL_CONTENT_URI
+        // as a fake uri here.
+        intent.setDataAndType(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, "image/png");
+        List<ResolveInfo> installedAppsInfo = mPm.queryIntentActivities(intent, 0);
+        for (ResolveInfo info : installedAppsInfo) {
+            ApplicationInfo appInfo = info.activityInfo.applicationInfo;
+            final PackageItem item = new PackageItem(appInfo.packageName,
+                    appInfo.loadLabel(mPm), appInfo.loadIcon(mPm));
+            mHandler.obtainMessage(0, item).sendToTarget();
+        }
+    }
+
+    private static class ViewHolder {
+        TextView title;
+        ImageView icon;
+    }
+}

Atomic-OS/platform_packages_apps_Laboratory

Signed-off-by: AKabhishek <abhishekkaushik.star@gmail.com>

src/com/aos/laboratory/fragments/Miscellaneous.java
@@ -16,10 +16,13 @@
 
 package com.aos.laboratory.fragments;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.content.Context;
 import android.content.ContentResolver;
 import android.content.res.Resources;
 import android.os.Bundle;
+import android.os.Handler;
 import android.os.UserHandle;
 import android.support.v7.preference.PreferenceCategory;
 import android.support.v7.preference.ListPreference;
@@ -34,20 +37,30 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ListView;
+import android.widget.Toast;
+
 import com.android.internal.utils.du.DUActionUtils;
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
 import com.android.internal.logging.nano.MetricsProto;
+import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
+
+import com.aos.laboratory.preferences.ScreenshotEditPackageListAdapter;
+import com.aos.laboratory.preferences.ScreenshotEditPackageListAdapter.PackageItem;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class Miscellaneous extends SettingsPreferenceFragment implements
-        Preference.OnPreferenceChangeListener {
+        Preference.OnPreferenceChangeListener, Preference.OnPreferenceClickListener {
 
     private static final String HEADSET_CONNECT_PLAYER = "headset_connect_player";
     private static final String TORCH_POWER_BUTTON_GESTURE = "torch_power_button_gesture";
     private static final String SCREEN_OFF_ANIMATION = "screen_off_animation";
+    private static final int DIALOG_SCREENSHOT_EDIT_APP = 1;
 
     private ListPreference mLaunchPlayerHeadsetConnection;
     private ListPreference mTorchPowerButton;
@@ -55,6 +68,8 @@ public class Miscellaneous extends SettingsPreferenceFragment implements
 
     private PreferenceCategory mLedsCategory;
     private Preference mChargingLeds;
+    private Preference mScreenshotEditAppPref;
+    private ScreenshotEditPackageListAdapter mPackageAdapter;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -62,6 +77,10 @@ public class Miscellaneous extends SettingsPreferenceFragment implements
               addPreferencesFromResource(R.xml.MiscPrefs);
               ContentResolver resolver = getActivity().getContentResolver();
               final PreferenceScreen prefSet = getPreferenceScreen();
+
+        mPackageAdapter = new ScreenshotEditPackageListAdapter(getActivity());
+        mScreenshotEditAppPref = findPreference("screenshot_edit_app");
+        mScreenshotEditAppPref.setOnPreferenceClickListener(this);
 
         mLaunchPlayerHeadsetConnection = (ListPreference) findPreference(HEADSET_CONNECT_PLAYER);
         int mLaunchPlayerHeadsetConnectionValue = Settings.System.getIntForUser(resolver,
@@ -103,6 +122,43 @@ public class Miscellaneous extends SettingsPreferenceFragment implements
         }
 
    }
+
+    @Override
+    public Dialog onCreateDialog(int dialogId) {
+        switch (dialogId) {
+            case DIALOG_SCREENSHOT_EDIT_APP: {
+                Dialog dialog;
+                AlertDialog.Builder alertDialog = new AlertDialog.Builder(getActivity());
+                final ListView list = new ListView(getActivity());
+                list.setAdapter(mPackageAdapter);
+                alertDialog.setTitle(R.string.profile_choose_app);
+                alertDialog.setView(list);
+                dialog = alertDialog.create();
+                list.setOnItemClickListener(new OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                        // Add empty application definition, the user will be able to edit it later
+                        PackageItem info = (PackageItem) parent.getItemAtPosition(position);
+                        Settings.System.putString(getActivity().getContentResolver(),
+                                Settings.System.SCREENSHOT_EDIT_USER_APP, info.packageName);
+                        dialog.cancel();
+                    }
+                });
+                return dialog;
+            }
+         }
+        return super.onCreateDialog(dialogId);
+    }
+
+    @Override
+    public int getDialogMetricsCategory(int dialogId) {
+        switch (dialogId) {
+            case DIALOG_SCREENSHOT_EDIT_APP:
+                return MetricsEvent.LABORATORY;
+            default:
+                return 0;
+        }
+    }
 
     @Override
     public int getMetricsCategory() {
@@ -152,4 +208,17 @@ public class Miscellaneous extends SettingsPreferenceFragment implements
         }
          return false;
       }
+
+    @Override
+    public boolean onPreferenceClick(Preference preference) {
+        // Don't show the dialog if there are no available editor apps
+        if (preference == mScreenshotEditAppPref && mPackageAdapter.getCount() > 0) {
+            showDialog(DIALOG_SCREENSHOT_EDIT_APP);
+        } else {
+            Toast.makeText(getActivity(), getActivity().getString(R.string.screenshot_edit_app_no_editor),
+                    Toast.LENGTH_LONG).show();
+        }
+        return true;
+    }
+
 }

src/com/aos/laboratory/preferences/ScreenshotEditPackageListAdapter.java
@@ -1,0 +1,151 @@
+/*
+ * Copyright (C) 2012-2014 The CyanogenMod Project
+ * Copyright (C) 2018 Dirty Unicorns
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aos.laboratory.preferences;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.graphics.drawable.Drawable;
+import android.os.Handler;
+import android.os.Message;
+import android.provider.MediaStore;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+
+import com.android.settings.R;
+
+public class ScreenshotEditPackageListAdapter extends BaseAdapter implements Runnable {
+    private PackageManager mPm;
+    private LayoutInflater mInflater;
+    private final List<PackageItem> mInstalledPackages = new LinkedList<PackageItem>();
+
+    private final Handler mHandler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            PackageItem item = (PackageItem) msg.obj;
+            int index = Collections.binarySearch(mInstalledPackages, item);
+            if (index < 0) {
+                mInstalledPackages.add(-index - 1, item);
+            }
+            notifyDataSetChanged();
+        }
+    };
+
+    public static class PackageItem implements Comparable<PackageItem> {
+        public final String packageName;
+        public final CharSequence title;
+        public final Drawable icon;
+
+        PackageItem(String packageName, CharSequence title, Drawable icon) {
+            this.packageName = packageName;
+            this.title = title;
+            this.icon = icon;
+        }
+
+        @Override
+        public int compareTo(PackageItem another) {
+            int result = title.toString().compareToIgnoreCase(another.title.toString());
+            return result != 0 ? result : packageName.compareTo(another.packageName);
+        }
+    }
+
+    public ScreenshotEditPackageListAdapter(Context context) {
+        mPm = context.getPackageManager();
+        mInflater = LayoutInflater.from(context);
+        reloadList();
+    }
+
+    @Override
+    public int getCount() {
+        synchronized (mInstalledPackages) {
+            return mInstalledPackages.size();
+        }
+    }
+
+    @Override
+    public PackageItem getItem(int position) {
+        synchronized (mInstalledPackages) {
+            return mInstalledPackages.get(position);
+        }
+    }
+
+    @Override
+    public long getItemId(int position) {
+        synchronized (mInstalledPackages) {
+            // packageName is guaranteed to be unique in mInstalledPackages
+            return mInstalledPackages.get(position).packageName.hashCode();
+        }
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ViewHolder holder;
+        if (convertView != null) {
+            holder = (ViewHolder) convertView.getTag();
+        } else {
+            convertView = mInflater.inflate(R.layout.applist_preference_icon, null, false);
+            holder = new ViewHolder();
+            convertView.setTag(holder);
+            holder.title = (TextView) convertView.findViewById(com.android.internal.R.id.title);
+            holder.icon = (ImageView) convertView.findViewById(R.id.icon);
+        }
+
+        PackageItem applicationInfo = getItem(position);
+        holder.title.setText(applicationInfo.title);
+        holder.icon.setImageDrawable(applicationInfo.icon);
+
+        return convertView;
+    }
+
+    private void reloadList() {
+        mInstalledPackages.clear();
+        new Thread(this).start();
+    }
+
+    @Override
+    public void run() {
+        Intent intent = new Intent();
+        intent.setAction(Intent.ACTION_EDIT);
+        // setData is needed because some apps like PhotoEditor need an uri for the EDIT intent action,
+        // otherwise they don't answer to the intent query. So we give them the generic EXTERNAL_CONTENT_URI
+        // as a fake uri here.
+        intent.setDataAndType(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, "image/png");
+        List<ResolveInfo> installedAppsInfo = mPm.queryIntentActivities(intent, 0);
+        for (ResolveInfo info : installedAppsInfo) {
+            ApplicationInfo appInfo = info.activityInfo.applicationInfo;
+            final PackageItem item = new PackageItem(appInfo.packageName,
+                    appInfo.loadLabel(mPm), appInfo.loadIcon(mPm));
+            mHandler.obtainMessage(0, item).sendToTarget();
+        }
+    }
+
+    private static class ViewHolder {
+        TextView title;
+        ImageView icon;
+    }
+}